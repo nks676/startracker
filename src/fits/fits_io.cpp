ff24e86c6b4bba3f12543691f5a1fdbe11cf3709
nks676 <narens11676@gmail.com>
2025-12-15 17:54:38 -0800

    created binary mask for potential star clusters. two-pass clustering algorithm to determine star clusters

--- src/fits/fits_io.cpp
+++ src/fits/fits_io.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "fits_io.h"
+#include <map>
+#include <cmath>
 #include "fitsio.h" // CFITSIO
 
 ImageData fits_to_data(const std::string& filename) {
@@ -11,14 +13,12 @@ ImageData fits_to_data(const std::string& filename) {
     int naxis = 0;
     long naxes[2] = {1, 1};
     
-    if (fits_open_file(&fptr, filename.c_str(), READONLY, &status) )
-    {
+    if (fits_open_file(&fptr, filename.c_str(), READONLY, &status) ) {
         fits_report_error(stderr, status);
         return data;
     }
 
-    if (fits_get_img_param(fptr, 2, &bitpix, &naxis, naxes, &status))
-    {
+    if (fits_get_img_param(fptr, 2, &bitpix, &naxis, naxes, &status)) {
         fits_report_error(stderr, status);
         fits_close_file(fptr, &status);
         return data;
@@ -37,11 +37,85 @@ ImageData fits_to_data(const std::string& filename) {
     long fpixel[2] = {1, 1};
     int anynul = 0;
 
-    if (fits_read_pix(fptr, TDOUBLE, fpixel, num_pixels, nullptr, data.pixels.data(), &anynul, &status))
-    {
+    if (fits_read_pix(fptr, TDOUBLE, fpixel, num_pixels, nullptr, data.pixels.data(), &anynul, &status)){
         fits_report_error(stderr, status);
         fits_close_file(fptr, &status);
         return data;
+    }
+
+    // mean
+    data.intensity_mean = 0.0;
+    for (double val: data.pixels) {
+        data.intensity_mean += val;
+    }
+    data.intensity_mean = data.intensity_mean / num_pixels;
+
+    // standard deviation
+    data.intensity_standard_deviation = 0.0;
+    for (double val: data.pixels) {
+        data.intensity_standard_deviation += (val - data.intensity_mean) * (val - data.intensity_mean);
+    }
+    data.intensity_standard_deviation = sqrt(data.intensity_standard_deviation / num_pixels);
+
+    // threshold and mask
+    double threshold_constant = 5.0;
+    data.intensity_threshold = data.intensity_mean + threshold_constant * data.intensity_standard_deviation;
+    data.pixels_mask.resize(num_pixels);
+    for (long i = 0; i < num_pixels; i++) {
+        data.pixels_mask[i] = (data.pixels[i] >= data.intensity_threshold);
+    }
+
+    // labeling using Union-Find
+    std::vector<int> labels(num_pixels, 0);
+    UnionFind uf(num_pixels / 2 + 1);
+    int label = 1;
+    for (long y = 0; y < data.height; ++y) {
+        for (long x = 0; x < data.width; ++x) {
+            long idx = y * data.width + x;
+            if (!data.pixels_mask[idx]) {
+                continue;
+            }
+
+            int left = (x > 0) ? labels[idx - 1] : 0;
+            int top = (y > 0) ? labels[idx - data.width] : 0;
+
+            if (left == 0 && top == 0) {
+                labels[idx] = label;
+                label++;
+            } else if (left != 0 && top == 0) {
+                labels[idx] = left;
+            } else if (left == 0 && top != 0) {
+                labels[idx] = top;
+            } else {
+                labels[idx] = left;
+                if (left != top) 
+                {
+                    uf.unite(left, top);
+                }
+            }
+        }
+    }
+
+    // resolve and cluster
+    std::map<int, int> label_to_vector;
+
+    for (long i = 0; i < num_pixels; ++i) {
+        if (labels[i] != 0) {
+            int root = uf.find(labels[i]);
+            labels[i] = root;
+
+            // create new cluster if it doesn't exist
+            if (label_to_vector.find(root) == label_to_vector.end()) {
+                label_to_vector[root] = data.clusters.size();
+                data.clusters.push_back(Cluster{root, {}});
+            }
+
+            int cluster_idx = label_to_vector[root];
+
+            long x = i % data.width;
+            long y = i / data.width;
+            data.clusters[cluster_idx].pixels.push_back(Pixel{x, y, data.pixels[i]});
+        }
     }
 
     fits_close_file(fptr, &status);
--- src/fits/fits_io.h
+++ src/fits/fits_io.h
@@ -3,11 +3,58 @@
 #include <vector>
 #include <string>
 
+struct Pixel
+{
+    long x;
+    long y;
+    double intensity;
+};
+
+struct Cluster
+{
+    int id;
+    std::vector<Pixel> pixels;
+};
+
 struct ImageData
 {
     std::vector<double> pixels;
     long width;
     long height;
+
+    double intensity_mean;
+    double intensity_standard_deviation;
+
+    double intensity_threshold;
+    std::vector<bool> pixels_mask; // 1 if star, 0 otherwise
+
+    std::vector<Cluster> clusters;
+};
+
+struct UnionFind
+{
+    std::vector<int> parent;
+    
+    UnionFind(int size) : parent(size) {
+        for (int i = 0; i < size; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    void unite(int x, int y) {
+        int rx = find(x);
+        int ry = find(y);
+        if (rx != ry) {
+            parent[ry] = rx;
+        }
+    }
 };
 
 ImageData fits_to_data(const std::string& filename);
